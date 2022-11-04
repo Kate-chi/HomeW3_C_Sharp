c04c31d0d68910029fe776c65c571a671f39cd51
Kate <c.chistova2014@yandex.ru>
2022-11-05 00:18:50 +0300

    кубы от 1 до N

---
+++ Task23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i <= num; i++)
+{
+    Console.Write(i * i * i);
+
+    if(i < num)
+    {
+        Console.Write(", ");
+    }
+}
